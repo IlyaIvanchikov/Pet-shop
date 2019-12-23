4a715e3eb303c18fdf979e28c31476d85ae1e2ab
Ilya Ivanchikov <phantomonster1@mail.ru>
2019-12-24 00:16:19 +0200

    feat: add 3 page

--- public/materializecss.js
+++ public/materializecss.js
@@ -4,5 +4,12 @@ $(document).ready(function () {
     $('.tabs').tabs();
   });
 
+const show = document.querySelector('.show')
+show.addEventListener('click', (event) => {
+  // let id = event.target.dataset.toggleId;
+  // if (!id) return;
 
-  console.log("alert");
+  let elem = document.getElementById('showId');
+
+  elem.hidden = !elem.hidden;
+})
